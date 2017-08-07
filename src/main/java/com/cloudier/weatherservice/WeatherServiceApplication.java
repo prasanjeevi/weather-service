5632f3a2c06f1928fe72c7ea67b42173968b4978
Sanjeevi <prasanjeevi@gmail.com>
2017-08-07 17:33:13 +0530

    basic circuit breaker with fallback

--- src/main/java/com/cloudier/weatherservice/WeatherServiceApplication.java
+++ src/main/java/com/cloudier/weatherservice/WeatherServiceApplication.java
@@ -2,11 +2,26 @@ package com.cloudier.weatherservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.netty.util.internal.ThreadLocalRandom;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class WeatherServiceApplication {
 
+	private String[] weather = new String[] { "sunny", "cloudy", "rainy", "windy" };
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherServiceApplication.class, args);
+	}
+	
+	@GetMapping("/weather")
+	public String getWeather() {
+		int rand = ThreadLocalRandom.current().nextInt(0, 4);
+		return weather[rand];
 	}
 }
